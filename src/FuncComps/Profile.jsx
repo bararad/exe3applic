9ea49efecbcb7cfe19513b0b71fdc65339e70a6d
bararad <bararad@gmail.com>
2024-02-19 15:43:22 +0200

    add profile

--- src/FuncComps/Profile.jsx
+++ src/FuncComps/Profile.jsx
@@ -4,38 +4,78 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 
 export default function Profile() {
-  let userInSession=JSON.parse(sessionStorage.getItem('connectedUser'));
-  console.log(userInSession);
-  
-  
+
+  let userInSession = JSON.parse(sessionStorage.getItem('connectedUser'));
+  console.log('usI', userInSession);
+
+  let adress = userInSession.userStreet + " " + userInSession.userHomeNum + "," + userInSession.userCity
+  let fullname = (userInSession.userFirstName + " " + userInSession.userLastName);
+  const onUpdateClick = () => {
+
+  }
+  const onToGameClick = () => {
+    <link href="" />
+
+  }
+  const onDisconnectClick = () => {
+
+  }
+
   return (
     <Card>
-      
-      {/* <CardContent>
-        <Typography variant="h5" component="div">
-          {userInSession}
-        </Typography>
-        <Typography color="text.secondary" gutterBottom>
-          Email: {email}
-        </Typography>
-        <Typography color="text.secondary" gutterBottom>
-          Address: {address}
-        </Typography>
-        <Typography color="text.secondary" gutterBottom>
-          Birth Date: {birthDate}
-        </Typography>
-        <Button variant="contained" onClick={onUpdateClick}>
-          Update
-        </Button>
-        <Button variant="contained" onClick={onToGameClick}>
-          To the Game
-        </Button>
-        <Button variant="contained" onClick={onDisconnectClick}>
-          Disconnect
-        </Button>
-      </CardContent> */}
+
+      {<CardContent style={{ display: 'flex', alignItems: 'center', backgroundColor: 'beige' }}>
+        <div>
+
+          <Typography variant="h2" component="div">
+            {fullname.toUpperCase()}
+
+            <Avatar
+              alt="User Avatar"
+              src={userInSession.userImage}
+              sx={{ width: 150, height: 150, marginRight: 5 }}
+            />
+          </Typography>
+        </div>
+        <div>
+          <Typography color="text.secondary" gutterBottom>
+            Email: {userInSession.userEmail}
+          </Typography>
+          <br />
+          <Typography color="text.secondary" gutterBottom>
+            Address: {adress}
+          </Typography>
+          <br />
+          <Typography color="text.secondary" gutterBottom>
+            Birth Date: {userInSession.userDofBirth}
+          </Typography>
+          <br />
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            '& button': { m: 1 }
+          }}>
+            <Button size='small' variant="contained"
+              sx={{ backgroundColor: 'grey' }}
+              onClick={onUpdateClick}>
+              Update
+            </Button>
+            <Button size='small' variant="contained" onClick={onToGameClick}>
+              To the Game
+            </Button>
+            <Button size='small' variant="contained"
+              sx={{ backgroundColor: 'red' }}
+              onClick={onDisconnectClick}>
+              Disconnect
+            </Button>
+          </Box>
+        </div>
+
+      </CardContent>}
     </Card>
   )
 }
