84a0c36795d9e3f2137e6c3b3eba0281c32326e5
bararad <bararad@gmail.com>
2024-02-20 11:32:34 +0200

    Update SystemAdmin.jsx
    system admin

--- src/FuncComps/SystemAdmin.jsx
+++ src/FuncComps/SystemAdmin.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Avatar, Button } from '@mui/material';
 
-export default function SystemAdmin() {
+export default function SystemAdmin(props) {
+  const users = props.users;
 
+  const createData = (image, username, fullname, dOfBirth, address, email) => {
+    return { image, username, fullname, dOfBirth, address, email };
+  };
+
+  const createRows = (users) => {
+    return users.map((user) => ({
+      image: user.userImage,
+      username: user.userName,
+      fullname: user.userFirstName + ' ' + user.userLastName,
+      dOfBirth: user.userDofBirth,
+      address: user.userStreet + ' ' + user.userHomeNum + ',' + user.userCity,
+      email: user.userEmail,
+    }));
+  };
+
+  const rows = createRows(users);
 
   return (
-    <div>SystemAdmin
-
-    </div>
-  )
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 1000 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Image</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell align="right">Full Name</TableCell>
+              <TableCell align="right">Date of Birth</TableCell>
+              <TableCell align="right">Address</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.username}>
+                <TableCell>
+                  <Avatar alt={row.username} src={row.image} />
+                </TableCell>
+                <TableCell component="th" scope="row">{row.username}</TableCell>
+                <TableCell align="right">{row.fullname}</TableCell>
+                <TableCell align="right">{row.dOfBirth}</TableCell>
+                <TableCell align="right">{row.address}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">
+                  <Button variant="contained" color="primary">
+                    Action
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
